de60dece6ca0d387af589acee489e83541067fc6
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-08-06 22:45:17 -0700

    Create _392_is_subsequence.java

---
+++ _392_is_subsequence.java
@@ -0,0 +1,22 @@
+package leetcode_problems;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class _392_is_subsequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Solution());
+	}
+
+	public static boolean Solution() {
+		
+		
+		
+		
+		return false;
+		
+
+	}
+}
