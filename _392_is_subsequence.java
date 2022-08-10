1d4a79390f2199f922ca01000d8b04e38275c7b1
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-08-09 21:46:41 -0700

    Update _392_is_subsequence.java

--- _392_is_subsequence.java
+++ _392_is_subsequence.java
@@ -6,17 +6,36 @@ import java.util.Arrays;
 public class _392_is_subsequence {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
+		
 		System.out.println(Solution());
 	}
 
 	public static boolean Solution() {
+
+		String s = "acb";
+		String t = "ahbgdc";
 		
+		if (s.length()==0) return true;
+		if(t.length() == 0) return true;
 		
+		int j = 0;
 		
+		for( int i = 0; i<t.length() ; i++) {
+			
+			if(s.charAt(j) == t.charAt(i) ) {
+				j++;
+				
+				if(j == s.length()) {
+					return true;
+				}
+			}
+			
+			
+		}
 		
 		return false;
-		
+
 
 	}
 }
